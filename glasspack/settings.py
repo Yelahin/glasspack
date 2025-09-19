ed556c0b5d8074e34a240406ea2049f765a62868
Illia_Yelahin <yelaginn@gmail.com>
2025-09-19 02:12:54 +0200

    CSRF_TRUSTED_ORIGINS add Ip of VPS for admin panel

--- glasspack/settings.py
+++ glasspack/settings.py
@@ -35,7 +35,8 @@ INTERNAL_IPS = [
 
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:8088",
-    "http://127.0.0.1:8088"
+    "http://127.0.0.1:8088",
+    "http://89.117.51.217"
 ]
 
 # Application definition
