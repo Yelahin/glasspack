2f35a137df5ea579290e42a2fb9d6c07da9646c6
Illia_Yelahin <yelaginn@gmail.com>
2025-11-05 13:42:53 +0100

    api for users

--- glasspack_api/serializers.py
+++ glasspack_api/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from glasspack_site.models import Product
 from glasspack_users.models import UserMessage
@@ -26,3 +27,23 @@ class UserMessageSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserMessage
         fields = ['full_name', 'email', 'comment']
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ["id", "username", "email", "password"]
+        extra_kwargs = {"password": {"write_only": True}}
+
+    def create(self, validated_data):
+        user = get_user_model().objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'], 
+            password=validated_data['password']
+        )
+        return user
+
+
+class UserLoginSerializer(serializers.Serializer):
+    old_password = serializers.CharField(required=True)
+    new_password = serializers.CharField(required=True)
--- glasspack_api/urls.py
+++ glasspack_api/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path, include
-from .views import ProductModelViewSet, UserMessageView
+from .views import ProductModelViewSet, UserMessageView, UserModelViewSet, me
 from rest_framework.routers import DefaultRouter
 
 
 router = DefaultRouter()
 router.register(r'products', ProductModelViewSet)
+router.register(r'users', UserModelViewSet)
 
 
 urlpatterns = [
     path('', include(router.urls)),
     path('contact/', UserMessageView.as_view()),
+    path('me/', me)
 ]
