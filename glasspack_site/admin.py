6a664bc959cac545bc071865fe096b234144e468
Illia <yelahinn@gmail.com>
2026-05-24 13:03:09 +0200

    feat: add not editable slug to admin page for models: Product, Category, Color, FinishType

--- glasspack_site/admin.py
+++ glasspack_site/admin.py
@@ -49,9 +49,10 @@ class VolumeFilter(admin.SimpleListFilter):
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
+    fields = ["name", "slug", "height", "weight", "diameter", "color", "finish_type", "categories", "image", "product_photo", "is_published", "time_create"]
     list_display = ('id', 'name', 'product_photo', 'time_create', 'is_published')
     list_display_links = ('id', 'name', 'time_create')
-    readonly_fields = ['product_photo']
+    readonly_fields = ['product_photo', 'slug', 'time_create']
     ordering = ('time_create',)
     list_editable = ('is_published',)
     list_per_page = 10
@@ -72,18 +73,26 @@ class ProductAdmin(admin.ModelAdmin):
 
 @admin.register(FinishType)
 class FinishTypeAdmin(admin.ModelAdmin):
+    fields = ["name", "slug"]
+    readonly_fields = ["slug"]
     list_display = ['name']
     list_display_links = ['name']
+    list_per_page = 10
 
 
 @admin.register(Color)
 class ColorAdmin(admin.ModelAdmin):
+    fields = ["name", "slug"]
+    readonly_fields = ["slug"]
     list_display = ['name']
     list_display_links = ['name']
+    list_per_page = 10
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
+    fields = ["name", "slug"]
+    readonly_fields = ["slug"]
     list_display = ('id', 'name')
     list_display_links = ('id', 'name')
     list_per_page = 10
